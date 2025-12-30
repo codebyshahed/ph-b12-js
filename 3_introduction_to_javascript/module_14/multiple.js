bdb67f086096358f64869ecf847c450215f466e9
alshahedbishwas <shahedevise@gmail.com>
2025-12-30 20:39:33 +0600

    14_5 Multiple condition Logical Operators(&&, ||)

---
+++ 3_introduction_to_javascript/module_14/multiple.js
@@ -0,0 +1,42 @@
+const salary = 15000;
+const isBCS = false;
+const height = 68;
+const hasCar = true;
+
+/*
+if (salary > 20000 && height > 66) {
+  console.log("Suuuu... patro");
+} else {
+  console.log("onno patro khuji");
+}
+*/
+
+/*
+if (salary > 25000 || height > 72 || isBCS) {
+  console.log("esoo babah.. kobul");
+} else {
+  console.log("vag tui mokbul");
+}
+*/
+
+/*
+if (salary > 25000 && height > 72 && isBCS) {
+  console.log("esoo babah.. kobul");
+} else {
+  console.log("vag tui mokbul");
+}
+*/
+
+/*
+if ((salary > 20000 && hasCar) || isBCS) {
+  console.log("tmr 14 gushthi raji");
+} else {
+  console.log("m*nger nati ja vag");
+}
+*/
+
+if ((salary > 20000 || hasCar) && isBCS) {
+  console.log("aho vatija aho...");
+} else {
+  console.log("khoniker pola.. ja vag");
+}
