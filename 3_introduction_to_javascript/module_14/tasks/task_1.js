10ef775fcbc5ab91e5ffce3f22a638bbaa25db4f
alshahedbishwas <shahedevise@gmail.com>
2026-01-01 19:48:48 +0600

    module 14-10 er task 1 done

---
+++ 3_introduction_to_javascript/module_14/tasks/task_1.js
@@ -0,0 +1,14 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+const burgurPrice = 550;
+
+if (burgurPrice >= 500) {
+  console.log("free coke");
+} else {
+  console.log("coke price 30 tk");
+}
