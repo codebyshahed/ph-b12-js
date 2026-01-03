0f0735dfc54bc8e8cf2cd1b9934aa15f49de5fe6
alshahedbishwas <shahedevise@gmail.com>
2026-01-03 23:10:34 +0600

    15_3 Problem solving with While Loop

---
+++ 3_introduction_to_javascript/module_15/js_loop/range.js
@@ -0,0 +1,14 @@
+let num = 1;
+
+while (num <= 10) {
+  console.log(num);
+  num++;
+}
+
+console.log("--------------------------");
+
+let number = 50;
+while (number <= 100) {
+  console.log(number);
+  number++;
+}
