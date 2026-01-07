590c7262b2994a14010efecd3d12a7f581710d09
alshahedbishwas <shahedevise@gmail.com>
2026-01-07 11:23:16 +0600

    js_loop_tasks er break section er 2 ta problem solve kore felsi but 3 number ta solve korte pari nai.. eita bad disi.. onk hard eita tai rki

---
+++ 3_introduction_to_javascript/module_15/js_loop/js_loop_tasks/break/3_square_break.js
@@ -0,0 +1,10 @@
+/* 
+Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+*/
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
+
+// eita BaseAudioContext.apply. eita bujio nai kori o nai
