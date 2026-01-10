c9d38fddb2d1b3680e1bf594e3d67f3fd75e99a6
codebyshahed <shahedevise@gmail.com>
2026-01-10 19:58:39 +0600

    trim() er video. 16_2 String Comparison lowercase uppercase and trim

---
+++ 3_introduction_to_javascript/module_16/js_string_object/case.js
@@ -0,0 +1,24 @@
+const school = "RAJ UK Uttara Model School";
+console.log(school); // mixed case
+// to lowercase
+console.log(school.toLowerCase()); // chot hater
+console.log(school.toUpperCase()); // boro hater
+// console.log(school);
+
+const subject = "Chemistry";
+const book = "ChemIsTry";
+
+if (subject.toLowerCase() === book.toLowerCase()) {
+  console.log("I am reading book.. eibar xm e fatai felmu");
+} else {
+  console.log("Hudda-hudi pristha ultai ar vat khawar jonno w8 kori");
+}
+
+const drink = " wa ter  ";
+const liquid = "water ";
+
+if (drink.trim() === liquid.trim()) {
+  console.log("panir opor name Life");
+} else {
+  console.log("somudre pani ache but khaite pari na");
+}
